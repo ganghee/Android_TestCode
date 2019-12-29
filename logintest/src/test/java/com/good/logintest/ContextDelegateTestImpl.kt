7894fb026e0dbe28f04e1202e4a8a0477abeae22
ganghee <idiosauri@naver.com>
2019-12-29 10:00:00 +0900

    updated code

--- logintest/src/test/java/com/good/logintest/ContextDelegateTestImpl.kt
+++ logintest/src/test/java/com/good/logintest/ContextDelegateTestImpl.kt
@@ -1,2 +1,13 @@
 package com.good.logintest
 
+import android.content.Context
+import com.good.logintest.delegate.ContextDelegate
+import org.mockito.Mockito.mock
+
+class ContextDelegateTestImpl : ContextDelegate{
+
+    private val mockContext = mock(Context::class.java)
+
+    override fun getString(resId: Int): String = mockContext.getString(resId)
+
+}
