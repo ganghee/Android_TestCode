2fd7fabaf8ee4982fbba7b1bd064fdb1ee27e8f3
ganghee <idiosauri@naver.com>
2020-01-04 10:00:00 +0900

    updated code

--- logintest/src/main/java/com/good/logintest/delegate/ContextDelegateImpl.kt
+++ logintest/src/main/java/com/good/logintest/delegate/ContextDelegateImpl.kt
@@ -1,2 +1,7 @@
 package com.good.logintest.delegate
 
+import android.content.Context
+
+class ContextDelegateImpl(private val context: Context?) : ContextDelegate {
+    override fun getString(resId: Int): String = context?.getString(resId)!!
+}
